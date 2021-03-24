a1edd6e271c1d59b4ec4bbc6ec4f048435ca1a6f
Kasia <71874062+KatarzynaBorkowska@users.noreply.github.com>
2021-03-24 12:51:24 +0100

    Initial Commit

---
+++ Sentence Searcher II/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Sentence Searcher II
+//
+//  Created by Kasia on 24/03/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
